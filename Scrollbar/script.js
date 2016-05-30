6adf503f3cfb35175209d22f579b359fc1a8a933
touma26 <ocramout@gmail.com>
2016-05-30 13:58:55 -0400

    JS pour augmenter la hauteur

--- Scrollbar/script.js
+++ Scrollbar/script.js
@@ -2,3 +2,10 @@ function my_fonction() {
 	user_name = prompt("Quel est votre nom ?");
 	document.getElementById('user_name').innerHTML = user_name;
 }
+function grandir(size) {
+	document.getElementById("grandir").setAttribute("size", size);
+		
+}
+function reduire(size) {
+	document.getElementById("grandir").setAttribute("size", size);
+}
