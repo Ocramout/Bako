7e3862e2f42ec52f3c4501d7c8c3878b89621575
touma26 <ocramout@gmail.com>
2016-05-31 15:47:23 -0400

    Rename

--- Scrollbar/script.js
+++ Scrollbar/script.js
@@ -2,6 +2,7 @@ function my_fonction() {
 	user_name = prompt("Quel est votre nom ?");
 	document.getElementById('user_name').innerHTML = user_name;
 }
+
 function grandir(size) {
 	document.getElementById("grandir").setAttribute("size", size);
 		
